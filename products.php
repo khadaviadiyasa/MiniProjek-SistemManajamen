b0d99c57400aa95b4a76565e40e2c89c31dbb2e5
khadaviadiyasa <khadaviadiyasa486@gmail.com>
2026-09-05 03:19:31 -1200

    Initial commit

--- products.php
+++ products.php
@@ -1,16 +1,52 @@
 <?php
 
 $Product = [
-    
-    "ID" => "I001",
-    "Nama" => "",
-    "Kategori" => "",
-    "Harga" => "",
-    "Stok" => "",
-    "Deskripsi" => "",
-    
-]
+   [ 
+    "ID" => "S001",
+    "Nama" => "AC",
+    "Kategori" => "Pendingin dan pengatur udara",
+    "Harga" => "Rp,2.300.000",
+    "Stok" => 10,
+    "Deskripsi" => "pendingin udara dalam ruangan yang dapat disesuaikan"
+   ],
+
+   [
+    "ID" => "S002",
+    "Nama" => "Kulkas",
+    "Kategori" => "Penyimpanan dan Pengolahan makanan",
+    "Harga" => "Rp,3.000.000",
+    "Stok" => 13,
+    "Deskripsi" => "Dilengkapi dengan pengaturan suhu, rak yang dapat disesuaikan, dan ruang penyimpanan yang luas, kulkas ini cocok digunakan untuk kebutuhan rumah tangga."
+   ],
+
+   [ 
+    "ID" => "S003",
+    "Nama" => "Rice cooker",
+    "Kategori" => "Penyimpanan dan Pengolahan makanan",
+    "Harga" => "Rp,300.000",
+    "Stok" => 8,
+    "Deskripsi" => "Rice Cooker Multifungsi adalah perangkat elektronik rumah tangga yang dirancang untuk memasak dan menjaga nasi tetap hangat secara praktis."
+   ],
+
+   [ 
+    "ID" => "S004",
+    "Nama" => "Mesin cuci",
+    "Kategori" => "Kebersihan dan Perawatan Pakaian",
+    "Harga" => "Rp,750.000",
+    "Stok" => 10,
+    "Deskripsi" => "Mesin ini dapat melakukan proses mencuci, membilas, dan mengeringkan pakaian secara otomatis sehingga menghemat waktu dan tenaga."
+   ],
+
+   [ 
+    "ID" => "S005",
+    "Nama" => "penyedot debu (vacuum cleaner)",
+    "Kategori" => "Kebersihan dan Perawatan Pakaian",
+    "Harga" => "Rp,250.000",
+    "Stok" => 15,
+    "Deskripsi" => "pembersih yang dirancang untuk membantu membersihkan debu, kotoran, dan partikel kecil dari berbagai permukaan seperti lantai, karpet, sofa, dan sudut ruangan."
+   ]
 
 
+];
 
-?>
+var_dump($Product);
